49ca9e92c49492755c07a7d5f59710f48aa375ea
Rail Sabirov <sabirov.rail@gmail.com>
2020-12-02 06:26:49 +0100

    Day 2

--- Program.cs
+++ Program.cs
@@ -11,8 +11,7 @@ namespace AdventOfCode2020
         [TestCase("Day1_test.txt", ExpectedResult = 514579)]
         public int Day1_1(string fileName)
         {
-            var filePath = Path.Combine(TestContext.CurrentContext.WorkDirectory, "../../../Input/", fileName);
-            var input = File.ReadAllLines(filePath).Select(int.Parse).ToArray();
+            var input = ReadAllLines(fileName).Select(int.Parse).ToArray();
 
             for (int i = 0; i < input.Length; i++)
             {
@@ -28,8 +27,7 @@ namespace AdventOfCode2020
         [TestCase("Day1_test.txt", ExpectedResult = 241861950)]
         public int Day1_2(string fileName)
         {
-            var filePath = Path.Combine(TestContext.CurrentContext.WorkDirectory, "../../../Input/", fileName);
-            var input = File.ReadAllLines(filePath).Select(int.Parse).ToArray();
+            var input = ReadAllLines(fileName).Select(int.Parse).ToArray();
 
             for (int i = 0; i < input.Length; i++)
             {
@@ -42,5 +40,42 @@ namespace AdventOfCode2020
             throw new InvalidOperationException("Solution not found");
         }
 
+        [TestCase("Day2_problem.txt", ExpectedResult = 550)]
+        [TestCase("Day2_test.txt", ExpectedResult = 2)]
+        public int Day2_1(string fileName)
+        {
+            var inputs = ReadAllLines(fileName)
+                .Select(s => s.Split(new[] { ' ', '-', ':'}, StringSplitOptions.RemoveEmptyEntries))
+                .Select(parts => (from: int.Parse(parts[0]), to: int.Parse(parts[1]), symbol: parts[2].Single(), password: parts[3]))
+                .ToArray();
+
+            var correctPasswordsCount = inputs
+                .Count(a => a.password.Count(c => c == a.symbol) >= a.from &&
+                            a.password.Count(c => c == a.symbol) <= a.to);
+
+            return correctPasswordsCount;
+        }
+
+        [TestCase("Day2_problem.txt", ExpectedResult = 634)]
+        [TestCase("Day2_test.txt", ExpectedResult = 1)]
+        public int Day2_2(string fileName)
+        {
+            var inputs = ReadAllLines(fileName)
+                .Select(s => s.Split(new[] { ' ', '-', ':' }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(parts => (pos1: int.Parse(parts[0]), pos2: int.Parse(parts[1]), symbol: parts[2].Single(), password: parts[3]))
+                .ToArray();
+
+            var correctPasswordsCount = inputs
+                .Where(a => a.password.Length >= a.pos1 && a.password.Length >= a.pos2)
+                .Count(a => a.password[a.pos1 - 1] == a.symbol ^ a.password[a.pos2 - 1] == a.symbol);
+
+            return correctPasswordsCount;
+        }
+
+        private static string[] ReadAllLines(string fileName)
+        {
+            var filePath = Path.Combine(TestContext.CurrentContext.WorkDirectory, "../../../Input/", fileName);
+            return File.ReadAllLines(filePath);
+        }
     }
 }
