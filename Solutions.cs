caa4c9f33726a5b720977c99b161c0895ffbb21a
Rail Sabirov <sabirov.rail@gmail.com>
2020-12-21 22:52:43 +0100

    Day 21 cleanup

--- Solutions.cs
+++ Solutions.cs
@@ -806,59 +806,87 @@ namespace AdventOfCode2020
 
             throw new NotImplementedException();
         }
-        
+
         [TestCase("Day21_test.txt", ExpectedResult = 5)]
         [TestCase("Day21_problem.txt", ExpectedResult = 2230)]
         public int Day21_1(string fileName)
         {
             var inputs = ReadAllLines(fileName);
 
-            var regex = new Regex(@"^((?<product>\s*[a-z]+)\s)+(\(contains ((?<allergen>[a-z]+)[,\s]*)*\))?$");
-            var items = new List<(HashSet<string> ingredients, HashSet<string> allergens)>();
-            foreach (var input in inputs)
-            {
-                var match = regex.Match(input);
-                var products = match.Groups["product"].Captures.Select(c => c.Value).ToHashSet();
-                var allergens = match.Groups["allergen"].Captures.Select(c => c.Value).ToHashSet();
-                
-                items.Add((products, allergens));
-            }
+            var solver = new Day21(inputs);
+            return solver.GetNonAllergicProductsCount();
+        }
+        
+        [TestCase("Day21_test.txt", ExpectedResult = "mxmxvkd,sqjhc,fvjkl")]
+        [TestCase("Day21_problem.txt", ExpectedResult = "qqskn,ccvnlbp,tcm,jnqcd,qjqb,xjqd,xhzr,cjxv")]
+        public string Day21_2(string fileName)
+        {
+            var inputs = ReadAllLines(fileName);
 
-            var allergensInProducts = new Dictionary<string, HashSet<string>>();
-            foreach (var item in items)
+            var solver = new Day21(inputs);
+            return solver.GetDangerousIngredients();
+        }
+
+        private sealed class Day21
+        {
+            private readonly Regex _regex = new Regex(@"^((?<product>\s*[a-z]+)\s)+(\(contains ((?<allergen>[a-z]+)[,\s]*)*\))?$");
+
+            private readonly List<(HashSet<string> ingredients, HashSet<string> allergens)> _products = new();
+            private readonly Dictionary<string, HashSet<string>> _allergensInProducts = new();
+
+            public Day21(string[] inputs)
             {
-                foreach (var allergen in item.allergens)
+                // parsing the input
+                foreach (var match in inputs.Select(s => _regex.Match(s)))
                 {
-                    var existingIngredients = allergensInProducts.GetOrCreate(allergen, _ => new HashSet<string>());
-                    var newIngredients = new HashSet<string>(item.ingredients);
-                    
-                    if (existingIngredients.Count == 0)
-                        existingIngredients.UnionWith(newIngredients);
-                    else
-                        existingIngredients.IntersectWith(newIngredients);
+                    var products = match.Groups["product"].Captures.Select(c => c.Value).ToHashSet();
+                    var allergens = match.Groups["allergen"].Captures.Select(c => c.Value).ToHashSet();
+
+                    _products.Add((products, allergens));
+                }
+
+                // create allergens in ingredients
+                foreach (var item in _products)
+                {
+                    foreach (var allergen in item.allergens)
+                    {
+                        var existingIngredients = _allergensInProducts.GetOrCreate(allergen, _ => new HashSet<string>());
+                        var newIngredients = new HashSet<string>(item.ingredients);
+
+                        if (existingIngredients.Count == 0)
+                            existingIngredients.UnionWith(newIngredients);
+                        else
+                            existingIngredients.IntersectWith(newIngredients);
+                    }
                 }
             }
 
-            var allProducts = items.SelectMany(i => i.ingredients);
-            var allergicProducts = allergensInProducts.SelectMany(kv => kv.Value).ToHashSet();
-            
-            //return allProducts.Count(p => !allergicProducts.Contains(p));
-            var result = new List<(string ingr, string allerg)>();
-            while (allergensInProducts.Count > 0)
+            public int GetNonAllergicProductsCount()
             {
-                var kv = allergensInProducts.First(kv => kv.Value.Count == 1);
-                var ingredient = kv.Value.First();
+                var allProducts = _products.SelectMany(i => i.ingredients);
+                var allergicProducts = _allergensInProducts.SelectMany(kv => kv.Value).ToHashSet();
 
-                allergensInProducts.Remove(kv.Key);
-                foreach (var allergensInProduct in allergensInProducts) 
-                    allergensInProduct.Value.Remove(ingredient);
-
-                result.Add((ingr: ingredient, allerg: kv.Key));
+                return allProducts.Count(p => !allergicProducts.Contains(p));
             }
 
-            var a = string.Join(",", result.OrderBy(kv => kv.allerg).Select(kv => kv.ingr));
+            public string GetDangerousIngredients()
+            {
+                var result = new List<(string ingr, string allerg)>();
+                while (_allergensInProducts.Count > 0)
+                {
+                    var kv = _allergensInProducts.First(kv => kv.Value.Count == 1);
+                    var ingredient = kv.Value.First();
+                    var allergen = kv.Key;
 
-            return 0;
+                    _allergensInProducts.Remove(allergen);
+                    foreach (var allergensInProduct in _allergensInProducts)
+                        allergensInProduct.Value.Remove(ingredient);
+
+                    result.Add((ingr: ingredient, allerg: allergen));
+                }
+
+                return string.Join(",", result.OrderBy(kv => kv.allerg).Select(kv => kv.ingr));
+            }
         }
 
         private sealed class Day20Tile
@@ -879,7 +907,7 @@ namespace AdventOfCode2020
                         Data[i, j] = rawData[i + 1][j] == '#';
 
                 int a1 = 0, b1 = 0, c1 = 0, d1 = 0;
-                int a2 = 0, b2 = 0, c2= 0, d2 = 0;
+                int a2 = 0, b2 = 0, c2 = 0, d2 = 0;
                 for (int i = 0; i < size; i++)
                 {
                     if (Data[i, 0])
